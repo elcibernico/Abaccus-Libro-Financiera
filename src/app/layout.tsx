94216598a4d901320a2292b2a697408992172677
elcibernico <elcibernico@email.com>
2026-05-31 17:20:30 -0300

    fix(seo): remove default next.js favicon.ico and configure native dynamic light/dark favicons in metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,19 @@ import config from "../../data_content/locales/config.json";
 export const metadata: Metadata = {
   title: config.titletag.title,
   description: config.header.titleLine3,
+  icons: {
+    icon: [
+      {
+        url: config.titletag.faviconLightModeUrl,
+        media: "(prefers-color-scheme: light)",
+      },
+      {
+        url: config.titletag.faviconDarkModeUrl,
+        media: "(prefers-color-scheme: dark)",
+      }
+    ],
+    shortcut: config.titletag.faviconLightModeUrl,
+  }
 };
 
 export default function RootLayout({
