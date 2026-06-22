433a1fe7abf1246675dd61688af4b849dcc3f524
elcibernico <elcibernico@email.com>
2026-06-22 20:00:33 -0300

    chore: actualizar titulo en U04_Pto03-1-1-1-3.ts

--- src/modules/libro_financiero/data/U04_Pto03-1-1-1-3.ts
+++ src/modules/libro_financiero/data/U04_Pto03-1-1-1-3.ts
@@ -4,7 +4,8 @@ const data = {
   "Desarrollo": [
     {
       "type": "text",
-      "content": `<u><b>Valores que puede tomar la razón </b></u>$r$- Si $r>0⇒$ cuotas crecientes.
+      "content": `<u><b>Valores que puede tomar la razón </b></u>$r$
+- Si $r>0⇒$ cuotas crecientes.
 - Si $r=0⇒$ cuotas constantes y se obtiene la fórmula particular de las rentas temporarias inmediatas con cuotas constantes.
 - La razón $r$ puede ser negativa, en cuyo caso las cuotas son decrecientes, pero con una restricción: la última cuota debe ser positiva. Como las cuotas son decrecientes, si se verifica que la última cuota sea positiva, las anteriores también lo serán. Por consiguiente:
  $$c_{n}=c+\\left( n-1 \\right)\\cdot r>0$$
